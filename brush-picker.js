16d9875eff785440c9bb325f534147a62e823e69
Sam Saccone <samccone@gmail.com>
2016-03-28 21:12:50 -0700

    More batch painting operations

--- brush-picker.js
+++ brush-picker.js
@@ -5,26 +5,32 @@
 
   proto.attachedCallback = function() {
     let ul = document.createElement('ul');
+    let size = 50;
     let emojiKeys = Object.keys(emojiMap);
-    let width = Math.floor(window.innerWidth / 50);
-    let emojiRows = Math.floor(emojiKeys.length / width);
+    let width = Math.floor(window.innerWidth / size);
+    let rows = 8;
+    let emojisPerChunk = (width * rows);
+    let emojiRows = Math.floor(emojiKeys.length / emojisPerChunk);
+
 
     for(var i = 0; i < emojiRows; ++i) {
       let canvas = document.createElement('canvas');
       let li = document.createElement('li');
 
       canvas.setAttribute('width', window.innerWidth + 'px');
-      canvas.setAttribute('height', '50px');
+      canvas.setAttribute('height', `${size * rows}px`);
       let ctx = canvas.getContext('2d');
-      ctx.font = '50px Arial'
+      ctx.font = `${size}px Arial`
 
-      for(var j = 0; j < width; ++j) {
-        try {
-          ctx.fillText(
-            String.fromCodePoint(emojiMap[emojiKeys[
-              (i * width) + j
-            ]]), j * 50, (50));
-        } catch (e) {
+      for (var y = 0; y < rows; ++y) {
+        for(var x = 0; x < width; ++x) {
+          try {
+            ctx.fillText(
+              String.fromCodePoint(emojiMap[emojiKeys[
+                (i * emojisPerChunk) + (y * width) + x
+              ]]), x * size, size * (y + 1));
+          } catch (e) {
+          }
         }
       }
 
