194f65a6214a34f51bb251102c92767c6dda2ded
Jory Phillips <jory.l.phillips@gmail.com>
2016-05-21 16:45:07 -0700

    moji-brush episode VI: return of the hamburger

--- src/footer-menu.js
+++ src/footer-menu.js
@@ -6,7 +6,8 @@
   proto.template = _ => {
      return `<ul class="menu-items">
         <li class="dashboard" action="dashboard-menu">
-          <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet"><g id="dashboard"><path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"/></g></svg>
+          <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet"><g id="menu"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></g>
+          </svg>
         </li>
         <li class="size" action="size">
           <svg id="plusminus" viewBox="0 0 21 17">
