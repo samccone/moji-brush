ea727e2721a48fc0c085605d7e49f36e89e3564c
Jory Phillips <jory.l.phillips@gmail.com>
2016-05-24 18:13:17 -0700

    First build process complete, works in Safari now :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,39 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require("gulp-sourcemaps");
-var concat = require("gulp-concat");
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var postcss = require('gulp-postcss');
+var cssnano = require('cssnano');
+var autoprefixer = require('autoprefixer');
+var replace = require('gulp-replace');
 
-gulp.task("babelify", function () {
-  return gulp.src("src/**/*.js")
-    .pipe(sourcemaps.init())
+gulp.task('babelify', function () {
+  return gulp.src('src/**/*.js')
     .pipe(babel())
-    .pipe(concat("all.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('autoprefix', function () {
+gulp.task('css', function () {
+  var processors = [
+    autoprefixer({browsers: ['last 2 versions']}),
+    cssnano(),
+];
 	return gulp.src('src/app.css')
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
+    .pipe(postcss(processors))
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['babelify', 'autoprefix'], function() {});
+gulp.task('images', function () {
+  return gulp.src(['src/images/**', '!**/.DS_Store'])
+    .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('vendor', function () {
+  return gulp.src('src/vendor/**')
+    .pipe(gulp.dest('dist/vendor'));
+});
+
+gulp.task('default', ['babelify', 'css', 'images', 'html', 'vendor'], function() {});
