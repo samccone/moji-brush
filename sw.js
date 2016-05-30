c5f0d9336bcbd9a238c3be0f9e774e7ea40778c7
Sam Saccone <samccone@gmail.com>
2016-05-30 13:36:30 -0700

    🎄 Deploy Mon May 30 13:36:30 PDT 2016

--- sw.js
+++ sw.js
@@ -1,34 +1,44 @@
-'use strict';
-
 var VERSION = '0.0.9';
 
-undefined.addEventListener('install', function (e) {
-  e.waitUntil(caches.open(VERSION).then(function (cache) {
-    return cache.addAll(['/', '/index.html', '/app.css', '/app.js', '/footer-menu.js', '/page-menu.js', '/draw-canvas.js', '/brush-picker-pane.js', '/emoji-map.js', '/size-picker.js', '/background-overlay.js', '/welcome.json', '/manifest.json']);
-  }));
-});
+this.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(VERSION).then(cache => {
+    return cache.addAll([
+      '/',
+      '/index.html',
+      '/app.css',
+      '/app.js',
+      '/footer-menu.js',
+      '/page-menu.js',
+      '/draw-canvas.js',
+      '/brush-picker-pane.js',
+      '/emoji-map.js',
+      '/size-picker.js',
+      '/background-overlay.js',
+      '/welcome.json',
+      '/manifest.json',
+    ]);
+}))});
 
-undefined.addEventListener('fetch', function (e) {
-  e.respondWith(caches.match(e.request).catch(function (_) {
+this.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).catch(_ => {
     return handleNoCacheMatch();
-  }));
+  }))
 });
 
-undefined.addEventListener('activate', function (e) {
-  e.waitUntil(caches.keys().then(function (keys) {
-    return Promise.all(keys.map(function (k) {
+this.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.map(k => {
       if (k !== VERSION) {
         return caches.delete(k);
       }
     }));
-  }));
-});
+}))});
 
 // fetch from network
 // and put into our cache
 function handleNoCacheMatch(e) {
-  return fetch(e.request).then(function (res) {
-    return caches.open(VERSION).then(function (cache) {
+  return fetch(e.request).then(res => {
+    return caches.open(VERSION).then(cache => {
       cache.put(e.request, res.clone());
 
       return res;
