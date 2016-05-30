8172bd61acc035b0ac3ee97eac3b7e3c6f0dfdf7
Sam Saccone <samccone@gmail.com>
2016-05-30 12:51:24 -0700

    Fixup gulpfile formatting

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,33 @@ var replace = require('gulp-replace');
 
 gulp.task('babelify', function () {
   return gulp.src('src/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
+  .pipe(babel())
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('css', function () {
   var processors = [
     autoprefixer({browsers: ['last 2 versions']}),
     cssnano(),
-];
-	return gulp.src('src/app.css')
-    .pipe(postcss(processors))
-		.pipe(gulp.dest('dist'));
+  ];
+  return gulp.src('src/app.css')
+  .pipe(postcss(processors))
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', function () {
   return gulp.src(['src/images/**', '!**/.DS_Store'])
-    .pipe(gulp.dest('dist/images/'));
+  .pipe(gulp.dest('dist/images/'));
 });
 
 gulp.task('html', function () {
   return gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('vendor', function () {
   return gulp.src('src/vendor/**')
-    .pipe(gulp.dest('dist/vendor'));
+  .pipe(gulp.dest('dist/vendor'));
 });
 
 gulp.task('default', ['babelify', 'css', 'images', 'html', 'vendor'], function() {});
