2636cff5a12df801da89ce569dedf1e129ac3d6d
Jory Phillips <jory.l.phillips@gmail.com>
2016-06-11 07:56:39 -0700

    emoji pruning

--- src/emoji-map.js
+++ src/emoji-map.js
@@ -2,11 +2,11 @@ const emojiMap = {
     "apple": {
         "black": ["1f393.png", "1f3a5.png", "1f3a9.png", "1f3ae.png", "1f3b1.png", "1f403.png", "1f41c.png", "1f4de.png", "1f50c.png"],
 
-        "blue": ["1f4d8.png", "1f5fe.png", "1f698.png", "1f6e2.png", "2693.png"],
+        "blue": ["1f4d8.png", "1f698.png", "1f6e2.png", "2693.png"],
 
-        "brown-dark": ["1f330.png", "1f357.png", "1f369.png", "1f3c8.png", "1f45c.png", "1f45e.png", "1f462.png", "1f4a9.png", "1f982.png"],
+        "brown-dark": ["1f330.png", "1f357.png", "1f369.png", "1f3c8.png", "1f45c.png", "1f45e.png", "1f4a9.png", "1f982.png"],
 
-        "blue-light": ["1f3bd.png", "1f433.png", "1f455.png", "1f4a7.png", "1f5fd.png", "2744.png"],
+        "blue-light": ["1f3bd.png", "1f433.png", "1f455.png", "1f4a7.png", "2744.png"],
 
         "brown-light": ["1f36a.png", "1f3c6.png", "1f3c9.png", "1f42a.png", "1f43b.png", "1f4e6.png", "1f6aa.png"],
 
@@ -14,13 +14,13 @@ const emojiMap = {
 
         "green-light": ["1f340.png", "1f343.png", "1f34f.png", "1f438.png", "1f4d7.png", "2618.png", "267b.png"],
 
-        "grey-dark": ["1f311.png", "1f399.png", "1f529.png", "1f58a.png", "1f68a.png", "2712.png"],
+        "grey-dark": ["1f311.png", "1f399.png", "1f529.png", "1f58a.png", "1f68a.png"],
 
         "grey-light": ["1f368.png", "1f3d0.png", "1f40f.png", "1f516.png", "1f527.png", "1f56f.png", "1f5d1.png", "1f5dc.png"],
 
-        "grey-medium": ["1f480.png", "1f400.png", "1f517.png", "1f58b.png", "2620.png", "2694.png", "2699.png", "26d3.png"],
+        "grey-medium": ["1f480.png", "1f400.png", "1f517.png", "1f58b.png", "2620.png", "2694.png", "26d3.png"],
 
-        "indigo": ["1f30c.png", "1f346.png", "1f3d9.png", "1f456.png", "1f699.png", "1f6cb.png", "2602.png", "2614.png"],
+        "indigo": ["1f346.png", "1f456.png", "1f699.png", "1f6cb.png", "2602.png", "2614.png"],
 
         "orange": ["1f32d.png", "1f34a.png", "1f383.png", "1f3c0.png", "1f458.png"],
 
@@ -38,7 +38,7 @@ const emojiMap = {
 
         "blue": ["1f41f.png", "1f5f3.png", "1f68e.png", "1f6e1.png", "2708.png"],
 
-        "blue-light": ["1f4a7.png", "1f516.png", "2604.png", "2668.png", "2693.png", "2744.png"],
+        "blue-light": ["1f4a7.png", "1f516.png", "2668.png", "2693.png", "2744.png"],
 
         "brown-dark": ["1f3c8.png", "1f40e.png", "1f43b.png", "1f45e.png", "1f4a9.png", "1f4ff.png"],
 
@@ -50,15 +50,15 @@ const emojiMap = {
 
         "grey-dark": ["1f39e.png", "1f4f0.png", "1f577.png", "1f578.png"],
 
-        "grey-light": ["1f3d0.png", "1f3db.png", "1f54a.png", "1f56f.png", "1f5a5.png", "1f5d2.png"],
+        "grey-light": ["1f3d0.png", "1f3db.png", "1f54a.png", "1f56f.png", "1f5d2.png"],
 
         "grey-medium": ["1f399.png", "1f400.png", "1f403.png", "1f480.png", "1f6e2.png"],
 
         "indigo": ["1f302.png", "1f40b.png", "1f418.png", "1f42c.png", "1f456.png", "1f6b0.png", "260e.png"],
 
-        "orange": ["1f342.png", "1f3a8.png", "1f3c9.png", "1f40c.png", "1f420.png", "1f431.png", "1f434.png", "1f436.png", "1f439.png", "2638.png"],
+        "orange": ["1f342.png", "1f3c9.png", "1f420.png", "1f431.png", "1f434.png", "2638.png"],
 
-        "red": ["1f336.png", "1f337.png", "1f339.png", "1f39f.png", "1f58d.png", "1f608.png", "1f980.png", "26d1.png"],
+        "red": ["1f336.png", "1f337.png", "1f339.png", "1f39f.png", "1f58d.png", "1f608.png", "1f980.png"],
 
         "pink": ["1f338.png", "1f33c.png", "1f346.png", "1f347.png", "1f351.png", "1f429.png", "1f457.png", "1f460.png", "1f47e.png"],
 
