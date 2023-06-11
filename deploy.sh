fae1af6b202e101cb98023c4e094e9a1085a5001
Sam Saccone <samccone@sams-mac-studio.lan>
2023-06-11 13:16:55 -0700

    Fix build script to work withe modern git + master to main rename

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ set -e
 
 npm run build
 cd dist
-git init
+git init --initial-branch=master
 git remote add origin git@github.com:samccone/moji-brush.git
 git fetch
 git add .
 git commit -m "🎄 Deploy $(date)"
 git checkout gh-pages
 git cherry-pick master --keep-redundant-commits --strategy-option theirs
-git push
+git push --set-upstream origin gh-pages
 cd ..
 rm -rf dist
